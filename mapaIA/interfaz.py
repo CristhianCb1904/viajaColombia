67d90e956315574102eef96661a28fa6d882627d
CristhianCb1904 <est.cristhian.cber2@unimilitar.edu.co>
2026-03-27 17:53:47 -0500

    Array de capitales

--- mapaIA/interfaz.py
+++ mapaIA/interfaz.py
@@ -1,3 +1,4 @@
+import numpy as np
 import customtkinter as ctk
 from tkintermapview import TkinterMapView
 
@@ -40,3 +41,18 @@ mapWidget.set_zoom(6)
 mapWidget.set_marker(4.5709, -74.0721, text="Bogotá")
 
 ventana.mainloop()
+
+
+#Capitales
+
+capitales = np.array([
+                    "Leticia", "Medellín", "Arauca", "Barranquilla", "Cartagena", "Tunja", "Manizales", "Florencia",
+                    "yopal", "Popayán", "Valledupar", "Quibdó", "Montería", "Bogotá", "Inírida", "San José del Guaviare",
+                    "Neiva", "Riohacha", "Santa Marta", "Villavicencio", "San Juan de Pasto", "San José de Cúcuta", 
+                    "Mocoa", "Armenia", "Pereira", "San Andrés", "Bucaramanga", "Sincelejo", "Ibagué", "Cali", "Mitú", "Puerto Carreño" ] )
+
+# Coordenadas de las capitales
+#coordenadas = np.array([[ -4.215, -69.940], [6.244, -75.573], [7.086, -70.759], [10.968, -74.781], [10.391, -75.479], [5.537, -73.367], [5.068, -75.517],
+#                        [1.613, -75.567], [2.454, -76.606], [2.444, -76.614], [10.463, -73.253], [5.694, -76.651], [8.753, -75.879], [4.5709, -74.2973], [2.585, -72.639],
+#                        [2.927, -75.280], [11.544, -72.907], [11.240, -74.199], [4.142, -73.629], [7.893, -72.507], [1.258, -77.281], [4.533, -75.681], [4.814, -75.696], [12.584, -81.700],
+#                        [7.119, -73.122], [4.438, -75.232], [3.451, -76.531], [1.252, -70.221], [6.186, -67.493] ])
