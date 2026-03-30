67e09ca475bcf9189aa1cb084182d59729eec1c8
CristhianCb1904 <est.cristhian.cber2@unimilitar.edu.co>
2026-03-29 20:35:10 -0500

    Capitales completas

--- mapaIA/interfaz.py
+++ mapaIA/interfaz.py
@@ -28,7 +28,33 @@ ctk.set_default_color_theme("blue")
 
 # coordenadas y grafos
 coordenadas = {
+    "Leticia": (-4.2153, -69.9408),
+    "Arauca": (7.0869, -70.7619),
+    "Barranquilla": (10.9685, -74.7813),
+    "Cartagena": (10.3910, -75.4794),
+    "Florencia": (1.6133, -75.5636),
+    "Yopal": (5.3372, -72.3848),
+    "Popayán": (2.4545, -76.6068),
+    "Valledupar": (10.4631, -73.2532),
+    "Quibdó": (5.6945, -76.6611),
+    "Montería": (8.7470, -75.8448),
     "Bogotá": (4.7110, -74.0721),
+    "Inírida": (3.8672, -67.9231),
+    "San José del Guaviare": (2.5689, -72.6394),
+    "Neiva": (2.9273, -75.2819),
+    "Riohacha": (11.5449, -72.9070),
+    "Santa Marta": (11.2408, -74.1990),
+    "Villavicencio": (4.1420, -73.6268),
+    "Pasto": (1.2136, -77.2819),
+    "Cúcuta": (7.8891, -72.4968),
+    "Mocoa": (1.1528, -76.6636),
+    "Armenia": (4.5339, -75.6811),
+    "San Andrés": (12.5833, -81.7000),
+    "Sincelejo": (9.3047, -75.3978),
+    "Ibagué": (4.4389, -75.2322),
+    "Cali": (3.4516, -76.5320),
+    "Mitú": (1.1983, -70.1733),
+    "Puerto Carreño": (6.1850, -67.4933),
     "Tunja": (5.5353, -73.3678),
     "Bucaramanga": (7.1193, -73.1227),
     "Medellín": (6.2442, -75.5812),
@@ -37,12 +63,201 @@ coordenadas = {
 }
 
 grafo = {
-    "Bogotá": {"Tunja": 2, "Manizales": 5},
-    "Tunja": {"Bogotá": 2, "Bucaramanga": 3},
-    "Bucaramanga": {"Tunja": 3},
-    "Manizales": {"Bogotá": 5, "Pereira": 1},
-    "Pereira": {"Manizales": 1, "Medellín": 4},
-    "Medellín": {"Pereira": 4}
+    
+    "Bogotá": {
+        "Tunja": 2,
+        "Villavicencio": 3,
+        "Ibagué": 4,
+        "Neiva": 5,
+        "Manizales": 6
+    },
+
+    "Tunja": {
+        "Bogotá": 2,
+        "Bucaramanga": 4,
+        "Yopal": 5
+    },
+
+    "Bucaramanga": {
+        "Tunja": 4,
+        "Cúcuta": 3,
+        "Arauca": 6,
+        "Medellín": 7
+    },
+
+    "Cúcuta": {
+        "Bucaramanga": 3,
+        "Arauca": 7
+    },
+
+    "Arauca": {
+        "Cúcuta": 7,
+        "Bucaramanga": 6,
+        "Yopal": 5
+    },
+
+    "Yopal": {
+        "Tunja": 5,
+        "Arauca": 5,
+        "Villavicencio": 4
+    },
+
+    "Villavicencio": {
+        "Bogotá": 3,
+        "Yopal": 4,
+        "San José del Guaviare": 6
+    },
+
+    "San José del Guaviare": {
+        "Villavicencio": 6,
+        "Florencia": 5,
+        "Inírida": 8,
+        "Mitú": 9
+    },
+
+    "Inírida": {
+        "San José del Guaviare": 8,
+        "Puerto Carreño": 7,
+        "Mitú": 8
+    },
+
+    "Puerto Carreño": {
+        "Inírida": 7,
+        "Yopal": 9
+    },
+
+    "Mitú": {
+        "Inírida": 8,
+        "San José del Guaviare": 9,
+        "Leticia": 10
+    },
+
+    "Leticia": {
+        "Mitú": 10
+    },
+
+    "Florencia": {
+        "San José del Guaviare": 5,
+        "Neiva": 4,
+        "Mocoa": 5
+    },
+
+    "Neiva": {
+        "Bogotá": 5,
+        "Florencia": 4,
+        "Ibagué": 3,
+        "Popayán": 6
+    },
+
+    "Ibagué": {
+        "Bogotá": 4,
+        "Neiva": 3,
+        "Armenia": 3,
+        "Manizales": 4,
+        "Cali": 5
+    },
+
+    "Manizales": {
+        "Bogotá": 6,
+        "Ibagué": 4,
+        "Pereira": 2,
+        "Medellín": 4
+    },
+
+    "Pereira": {
+        "Manizales": 2,
+        "Armenia": 2,
+        "Medellín": 5
+    },
+
+    "Armenia": {
+        "Pereira": 2,
+        "Ibagué": 3,
+        "Cali": 3
+    },
+
+    "Medellín": {
+        "Manizales": 4,
+        "Pereira": 5,
+        "Bucaramanga": 7,
+        "Montería": 5,
+        "Quibdó": 6
+    },
+
+    "Quibdó": {
+        "Medellín": 6,
+        "Cali": 6
+    },
+
+    "Cali": {
+        "Armenia": 3,
+        "Ibagué": 5,
+        "Popayán": 3,
+        "Quibdó": 6
+    },
+
+    "Popayán": {
+        "Cali": 3,
+        "Pasto": 4,
+        "Neiva": 6,
+        "Mocoa": 5
+    },
+
+    "Pasto": {
+        "Popayán": 4,
+        "Mocoa": 4
+    },
+
+    "Mocoa": {
+        "Pasto": 4,
+        "Popayán": 5,
+        "Florencia": 5
+    },
+
+    "Montería": {
+        "Medellín": 5,
+        "Sincelejo": 3,
+        "Cartagena": 5
+    },
+
+    "Sincelejo": {
+        "Montería": 3,
+        "Cartagena": 3,
+        "Barranquilla": 5
+    },
+
+    "Cartagena": {
+        "Sincelejo": 3,
+        "Barranquilla": 2,
+        "Montería": 5
+    },
+
+    "Barranquilla": {
+        "Cartagena": 2,
+        "Santa Marta": 2,
+        "Sincelejo": 5
+    },
+
+    "Santa Marta": {
+        "Barranquilla": 2,
+        "Riohacha": 3,
+        "Valledupar": 4
+    },
+
+    "Riohacha": {
+        "Santa Marta": 3,
+        "Valledupar": 4
+    },
+
+    "Valledupar": {
+        "Santa Marta": 4,
+        "Riohacha": 4,
+        "Bucaramanga": 6
+    },
+
+    "San Andrés": {
+    }
+
 }
 
 # busqueda por profundidad
@@ -162,6 +377,19 @@ def animar_ruta(ruta_real):
 
 def ejecutar_dfs():
     ruta = DFS(origen.get(), destino.get())
+    if origen.get() == destino.get():
+        label_resultado.configure(text="📍 Ruta:\n  • " + origen.get() + "\n\n📏 0 km\n⏱ 0 min")
+        return
+    elif origen.get() == "San Andrés" or destino.get() == "San Andrés":
+        label_resultado.configure(text="❌ No hay ruta disponible para San Andrés")
+        return
+    elif origen.get()=="" or destino.get()=="":
+        label_resultado.configure(text="❌ Por favor, seleccione ambos campos")
+        return
+    elif origen.get() == "Leticia" or destino.get() == "Leticia":
+        label_resultado.configure(text="❌ No hay ruta disponible para Leticia")
+        return
+    
     if ruta:
         r = dibujar_ruta(ruta)
         animar_ruta(r)
@@ -169,6 +397,19 @@ def ejecutar_dfs():
 
 def ejecutar_ucs():
     ruta, _ = UCS(origen.get(), destino.get())
+    if origen.get() == destino.get():
+        label_resultado.configure(text="📍 Ruta:\n  • " + origen.get() + "\n\n📏 0 km\n⏱ 0 min")
+        return
+    elif origen.get() == "San Andrés" or destino.get() == "San Andrés":
+        label_resultado.configure(text="❌ No hay ruta terrestre disponible para San Andrés")
+        return
+    elif origen.get()=="" or destino.get()=="":
+        label_resultado.configure(text="❌ Por favor, seleccione ambos campos")
+        return
+    elif origen.get() == "Leticia" or destino.get() == "Leticia":
+        label_resultado.configure(text="❌ No hay ruta terrestredisponible para Leticia")
+        return
+    
     if ruta:
         r = dibujar_ruta(ruta)
         animar_ruta(r)
